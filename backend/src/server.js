3dee6d7c72b0f8f2285a8e0bfb86f47e4d20653d
Ananya Rajawat <ananya23101@iiitnr.edu.in>
2026-06-22 22:16:05 +0530

    Remove error handling for uncaught exceptions
    Removed uncaught exception and unhandled rejection handlers.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,5 @@
 require('dotenv').config();
-// Catch hidden crashes
-process.on('uncaughtException', (err) => {
-  console.error('UNCAUGHT EXCEPTION', err);
-});
 
-process.on('unhandledRejection', (err) => {
-  console.error('UNHANDLED REJECTION', err);
-});
 
 const express = require('express');
 const cors = require('cors');
